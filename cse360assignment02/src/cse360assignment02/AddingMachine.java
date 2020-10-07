f04c96e56ce3618dbf39f60cc0def4e5ac50451f
Jake <Jake@njproctor>
2020-10-07 14:01:20 -0700

    First Version

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,65 @@
+/**
+ * Nicholas Proctor
+ * CSE360
+ * Assignment 02
+ */
+package cse360assignment02;
+
+/**
+ * 
+ * @author njproc
+ */
+public class AddingMachine {
+    private int total;
+    
+    
+    public static void main(String[] args) {
+    	
+    }
+    
+    /**
+     * 
+     */
+    public AddingMachine () {
+        total = 0; //included for clarity
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int getTotal () {
+        return 0;
+    }
+    
+    /**
+     * 
+     * @param value 
+     */
+    public void add (int value) {
+        
+    }
+    
+    /**
+     * 
+     * @param value 
+     */
+    public void subtract (int value) {
+        
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String toString () {
+        return "";
+    }
+    
+    /**
+     * 
+     */
+    public void clear() {
+        
+    }
+}
